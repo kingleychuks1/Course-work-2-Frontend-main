5f4793ea83628183c9a3ca1e27c78a7459734d86
kingleychuks1 <kingleychuks@gmail.com>
2024-02-20 12:54:07 +0000

    undo changes on http

--- js/main.js
+++ js/main.js
@@ -81,7 +81,7 @@ var app = new Vue ({
             alert('Cheers '+ user.name + ' ,your order has been successfully submitted');   
         },
         checkOut: function () {
-            fetch('https://kingleychuks1.github.io/Course-work-2-Frontend-main/', {
+            fetch('http://coursework-env.eba-qzgh8hw3.eu-west-2.elasticbeanstalk.com/collection/products/order', {
                 method: 'POST', // set the HTTP method as 'POST'
                 headers: {
                     'Content-Type': 'application/json', // set the data type as JSON
@@ -187,7 +187,7 @@ var app = new Vue ({
             
     },
     created() {
-        fetch("https://kingleychuks1.github.io/Course-work-2-Frontend-main/")
+        fetch("http://coursework-env.eba-qzgh8hw3.eu-west-2.elasticbeanstalk.com/collection/products")
         .then(response => response.json())
         .then(data => (this.lessons = data));
       }
